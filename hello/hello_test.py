8c1faef0750b7d782eb1ebb2041924b01947ecd9
Beeze <m.brown298@gmail.com>
2016-08-02 19:37:39 -0700

    Removed the test

--- hello/hello_test.py
+++ hello/hello_test.py
@@ -1,12 +1,12 @@
-import unittest
+# import unittest
 
-from .hello_app import app
+# from .hello_app import app
 
 
-class HelloTest(unittest.TestCase):
+# class HelloTest(unittest.TestCase):
 
-    def setUp(self):
-        self.app = app.test_client()
+#     def setUp(self):
+#         self.app = app.test_client()
 
-    def test_root(self):
-        assert 'Hello' in self.app.get('/').data
+#     def test_root(self):
+#         assert 'Hello' in self.app.get('/').data
