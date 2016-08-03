71a24df1322712354a7f3787f8b2b2deb25a61a8
Beeze <m.brown298@gmail.com>
2016-08-02 19:35:02 -0700

    Go

--- hello/hello_app.py
+++ hello/hello_app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Continuous Integrations. It always seems impossible until its done. - Nelson Mandela"
+    return "Continuous Integrations. It always seems impossible until its done. :Nelson Mandela"
 
 if __name__ == "__main__":
     app.run()
